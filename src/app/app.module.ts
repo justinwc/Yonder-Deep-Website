f348e8c3a96065654abc73c1ad38e683e67eef5f
justinwc <jwcho315@gmail.com>
2021-09-27 20:13:05 -0700

    adding files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,47 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { MissionComponent } from './mission/mission.component';
+import { ImpactComponent } from './impact/impact.component';
+import { DesignComponent } from './design/design.component';
+import { TeamsComponent } from './teams/teams.component';
+import { FooterComponent } from './footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatTabsModule} from '@angular/material/tabs';
+import { RecruitmentComponent } from './recruitment/recruitment.component';
+import {MatExpansionModule} from '@angular/material/expansion';
+import { MediaComponent } from './media/media.component';
+import {MatButtonToggleModule} from '@angular/material/button-toggle';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    MissionComponent,
+    ImpactComponent,
+    DesignComponent,
+    TeamsComponent,
+    FooterComponent,
+    RecruitmentComponent,
+    MediaComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatGridListModule,
+    MatTabsModule,
+    MatExpansionModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
